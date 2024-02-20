2414f0c8fb4c8f7ca9c130ab6e1c841befe67004
Yvaniak <67830211+Yvaniak@users.noreply.github.com>
2024-02-20 15:53:16 +0100

    new way of install

--- install-scripts/dotfiles.sh
+++ install-scripts/dotfiles.sh
@@ -12,35 +12,25 @@ source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
 printf "${NOTE} Downloading / Checking for existing Hyprland-Dots.tar.gz...\n"
 
 # Check if Hyprland-Dots.tar.gz exists
-if [ -f Hyprland-Dots ]; then
-  rm -r Hyprland-Dots
-fi
+# if [ -f Hyprland-Dots ]; then
+#   rm -r Hyprland-Dots
+# fi
 
 printf "${NOTE} Downloading the latest Hyprland commit...\n"
 
 # Download the latest release source code tarball to the current directory
-if git clone "git@github.com:Yvaniak/Hyprland-Dots.git"; then
-  cd "Hyprland-Dots" || exit 1
+# if git clone "git@github.com:Yvaniak/Hyprland-Dots.git"; then
+  cd "../Hyprland-Dots" || exit 1
 
   # Set execute permission for copy.sh and execute it
   chmod +x copy.sh
   ./copy.sh 
 
   echo -e "${OK} dots installed successfully" 2>&1 | tee -a "../Install-Logs/install-$(date +'%d-%H%M%S')_dotfiles.log"
-else
-  echo -e "${ERROR} Failed to download the latest commit." 2>&1 | tee -a "../Install-Logs/install-$(date +'%d-%H%M%S')_dotfiles.log"
-  exit 1
-fi
+# else
+#   echo -e "${ERROR} Failed to download the latest commit." 2>&1 | tee -a "../Install-Logs/install-$(date +'%d-%H%M%S')_dotfiles.log"
+#   exit 1
+# fi
 
-if [ -f Dots ]; then
-  cd Dots
-  stow .
-else
-  git clone "git@codeberg.org:Yvaniak/Dots.git"
-  cd Dots
-  stow .
-fi
-
-
-
-clear
+cd ..
+stow .
